7bb9199817ffe7f9abea9f86799f8692e9dfd81e
Rebecca Stone <you@example.com>
2022-02-09 17:11:44 -0600

    finished tasks 1,2, and 3 in article.js

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -115,3 +115,39 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+//might need to append the code-generated articles to the DOM
+const articleArea = document.querySelector(".articles");
+
+function articleMaker({title, date, firstParagraph, secondParagraph, thirdParagraph}){
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const paragraph1 = document.createElement('p');
+  const paragraph2 = document.createElement('p');
+  const paragraph3 = document.createElement('p');
+  const expandButton = document.createElement('span');
+
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate);
+  article.appendChild(paragraph1);
+  article.appendChild(paragraph2);
+  article.appendChild(paragraph3);
+  article.appendChild(expandButton);
+
+  article.classList.add("article");
+  articleDate.classList.add("date");
+  expandButton.classList.add("expandButton");
+
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  paragraph1.textContent = firstParagraph;
+  paragraph2.textContent = secondParagraph;
+  paragraph3.textContent = thirdParagraph;
+
+  expandButton.addEventListener("click", function(){
+    article.classList.toggle("article-open");
+  })
+  return article;
+}
+
